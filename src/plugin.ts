79784f96d6a9796d9decd89605dc164e1a1c0083
Chimildic <40138097+Chimildic@users.noreply.github.com>
2024-09-22 20:30:10 +0300

    Update: LogLevel to ERROR

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,6 +1,6 @@
 import streamDeck, { LogLevel } from "@elgato/streamdeck";
 import { ViewersAction } from "./actions/viewers";
 
-streamDeck.logger.setLevel(LogLevel.TRACE);
+streamDeck.logger.setLevel(LogLevel.ERROR);
 streamDeck.actions.registerAction(new ViewersAction());
 streamDeck.connect();
